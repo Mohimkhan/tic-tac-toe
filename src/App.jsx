a3e4fafa40a73409815f87e6e95be090ba3c1f10
Mohim Khan <mohimkhan652@gmail.com>
2024-01-12 07:42:19 +0600

    Add Board Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-/* eslint-disable react/prop-types */
-
-function Avater (props) {
-   let {title = 'hello everyone', des} = props.info;
-
-   console.log(title)
-
-   return <div className='bg-purple-500 text-white font-bold'>
-      <h1>{title}</h1>
-      <p>{des}</p>
-   </div>
-}
-
-function Profile () {
-   const info = {title: false, des: "It's recommended to destructure your props"}
-   return <Avater info={info}/>
-}
+import Board from "./components/Board";
 
 function App() {
    return (
       <>
-         <Profile />
+         <Board />
       </>
    );
 }
