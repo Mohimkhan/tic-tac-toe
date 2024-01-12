ede2c5a491955bc5351d89af88ec65b1be465948
Mohim Khan <mohimkhan652@gmail.com>
2024-01-12 19:15:34 +0600

    Add some styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({ btnType = 'button', children, ...rest }) => {
+const Button = ({ btnType = 'button', children = 'X', ...rest }) => {
    return (
-      <button type={btnType} {...rest}>
+      <button type={btnType} className="size-16 border border-gray-400 m-1 font-bold text-3xl" {...rest}>
          {children}
       </button>
    );
