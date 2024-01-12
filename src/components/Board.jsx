23f6a63c07c62609416e6c1f447a3187e5ef238c
Mohim Khan <mohimkhan652@gmail.com>
2024-01-12 19:20:40 +0600

    feat: add tic-tac-toe winner calculation utility function

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,27 +3,40 @@ import Square from './Square';
 
 const Board = () => {
    const [squares, setSquares] = useState(new Array(9).fill(null));
-   const [xIsNext, setXIsNext] = useState(true);
+   const [isXNext, setIsXNext] = useState(true);
+
+   const winner = calculateWinner(squares);
+
+   let status = '';
+
+   if (winner) {
+      status =  `Winner: ${winner}`
+   } else {
+      status = `Next Player: ${isXNext ? 'X' : 'O'}`;
+   }
 
    const handleClick = (squarePosition) => {
-      if (newSquare[squarePosition]) {
+      if (squares[squarePosition] || winner) {
          return;
       }
 
       const newSquare = [...squares];
 
-      if (xIsNext) {
+      if (isXNext) {
          newSquare[squarePosition] = 'X';
       } else {
          newSquare[squarePosition] = 'O';
       }
 
       setSquares(newSquare);
-      setXIsNext(!xIsNext);
+      setIsXNext(!isXNext);
    };
 
    return (
       <>
+      <div>
+         <h1>{status}</h1>
+      </div>
          <div className="flex items-center justify-center">
             <Square
                mark={squares[0]}
@@ -73,3 +86,24 @@ const Board = () => {
 };
 
 export default Board;
+
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
